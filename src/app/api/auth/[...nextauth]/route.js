dc2743d955cb1601155099d98cc50014657c5b2d
FeatureKreachure <43717996+FeatureKreachure@users.noreply.github.com>
2024-03-07 09:20:51 +0200

    small changes

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth/next";
 import authOptions from "../../../../utils/authOptions" 
 
-const handler = NextAuth(authOptions)
+export const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
